4ad52fcfaa2082587c58a56fcd8938dee77951dc
Miguel-Ma223 <miguel.bassbone@gmail.com>
2025-08-25 12:12:56 -0700

    Add files via upload

---
+++ reverseAstring.cpp
@@ -0,0 +1,35 @@
+// reverseAstring.cpp
+// mM 8/25/25
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+    cout << "\n\n Welcome to My Reverse a String Program! \n\n";
+
+    // Part 1
+    // Use the reverse() function from the library named <algorithm>
+
+    // Create a string that we will reverse
+    string myStrToReverse = "A Long String to Reverse";
+
+   string myEasyReversedStr;
+
+    cout << "\n The string before being reversed is: " << myStrToReverse;
+
+    reverse(myStrToReverse.begin(), myStrToReverse.end());
+
+    cout << "\n My reversed string is: " << myStrToReverse;
+
+    // Part 2
+    // Use a for loop to swap characters
+    for (int i=0; i<100; i++) {
+        cout << "\n the value of i is " << i << "\n";
+    }
+
+    return 0;
+}
